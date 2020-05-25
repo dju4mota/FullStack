dd9ab5f00a625db8f57686e6dae195c436550e23
dju4mota <fernando.b@ges.inatel.com>
2020-05-25 10:57:09 -0300

    Projeto Criado e Configurado com TypeScript e Nodemon

---
+++ Primeiro-Projeto-Node/src/server.ts
@@ -0,0 +1,11 @@
+import express, { json } from 'express';
+
+const app = express();
+
+app.get('/',(request, response) =>{
+  return response.json({message: 'Hello 🚀 !'});
+})
+
+app.listen(3333, () =>{
+  console.log('🚀Server Running')
+})
