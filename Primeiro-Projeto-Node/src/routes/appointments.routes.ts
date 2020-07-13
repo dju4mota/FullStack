b6c307c4cc5a43a83a481500502e2547d86fa5dc
dju4mota <fernando.b@ges.inatel.com>
2020-07-13 09:25:12 -0300

    Rota Appointments GET

--- Primeiro-Projeto-Node/src/routes/appointments.routes.ts
+++ Primeiro-Projeto-Node/src/routes/appointments.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
-import Appointment from '../models/Appointments';
+import { startOfHour, parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
+
+appointmentRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+  return response.json(appointments);
+});
 
 appointmentRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
